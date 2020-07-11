3aa36666366fb89ad6d261c993f34b70b840c870
Vishalnarvate <narvate.vi@gmail.com>
2020-07-11 13:47:46 +0530

    comment added add method

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Comment Main Main Activity
+        void add(){
+            System.out.println("hello");
+        }
     }
 }
